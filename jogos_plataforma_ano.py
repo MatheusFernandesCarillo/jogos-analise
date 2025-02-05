4b42ee8e4d21e473f24d8690c6edd82fa4461174
MatheusFernandesCarillo <75450635+MatheusFernandesCarillo@users.noreply.github.com>
2025-02-05 18:03:46 -0300

    Update jogos_plataforma_ano.py

--- jogos_plataforma_ano.py
+++ jogos_plataforma_ano.py
@@ -1,20 +1,22 @@
-import main
+from analise_ano import db_ano
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
-#ver qual plataforma mais teve jogos ao longo dos anos
-
+# Ver qual plataforma mais teve jogos ao longo dos anos
 db_plataforma_ano = db_ano.groupby(['Lançamento', 'Plataforma'])['Nome'].count().reset_index()
 db_plataforma_ano.rename(columns={'Nome': 'Quantidade de Jogos','Lançamento':' Ano de Lançamento'}, inplace=True)
 
-#ordena pela quantidade de jogos por plataforma, separando cada ano
+# Ordena pela quantidade de jogos por plataforma, separando cada ano
 db_plataforma_ano.sort_values(by='Quantidade de Jogos', ascending=False)
 
-#grafico mostrando a quantidade de jogos por plataforma, somados
+# Gráfico interativo, mostrando a quantidade de jogos por plataforma, ano a ano
+fig2 = px.line(db_plataforma_ano, x=' Ano de Lançamento', y='Quantidade de Jogos', color='Plataforma', title='Comparação de Plataformas ao Longo dos Anos')
+fig2.update_layout(xaxis_title='Ano de Lançamento', yaxis_title='Quantidade de Jogos')
+fig2.show()
+
+# Gráfico mostrando a quantidade de jogos por plataforma, somados
 plt.figure(figsize=(15, 8))
 sns.countplot(y="Plataforma", data=db_ano, order=db_ano['Plataforma'].value_counts().index)
 plt.title('Quantidade de Jogos por Plataforma')
 plt.show()
-
-#grafico interativ, mostrando a quantidade de jogos por plataforma, ano a ano
-fig = px.line(db_plataforma_ano, x=' Ano de Lançamento', y='Quantidade de Jogos', color='Plataforma', title='Comparação de Plataformas ao Longo dos Anos')
-fig.update_layout(xaxis_title='Ano de Lançamento', yaxis_title='Quantidade de Jogos')
-fig.show()
