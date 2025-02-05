35f024ebfd503a23365590f527c6d5c47f52808a
MatheusFernandesCarillo <75450635+MatheusFernandesCarillo@users.noreply.github.com>
2025-02-05 16:54:10 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+db = pd.read_csv('https://raw.githubusercontent.com/MatheusFernandesCarillo/games_datascience_study/refs/heads/main/Video_Games_Sales_as_at_22_Dec_2016.csv')
+
+db.columns = ["Nome", "Plataforma", "Lançamento", "Genero", "Publicadora", "Vendas_EUA", "Vendas_Europa", "Vendas_Japão", "Vendas_Outros", "Vendas_Global", "Nota_Critica", "Contagem_Criticos", "Nota_Usuario", "Contagem_Usuarios", "Desenvolvedora", "Classificação"]
+
+db_limpo = db.drop(columns=['Nota_Critica', 'Contagem_Criticos', 'Nota_Usuario', 'Contagem_Usuarios'])
