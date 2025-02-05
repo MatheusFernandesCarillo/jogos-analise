ba4c4b1d86f98a1a2fba3e6c77a99211e2686766
MatheusFernandesCarillo <75450635+MatheusFernandesCarillo@users.noreply.github.com>
2025-02-05 17:07:48 -0300

    Create jogos_plataforma_ano.py

---
+++ jogos_plataforma_ano.py
@@ -0,0 +1,20 @@
+import main
+
+#ver qual plataforma mais teve jogos ao longo dos anos
+
+db_plataforma_ano = db_ano.groupby(['Lançamento', 'Plataforma'])['Nome'].count().reset_index()
+db_plataforma_ano.rename(columns={'Nome': 'Quantidade de Jogos','Lançamento':' Ano de Lançamento'}, inplace=True)
+
+#ordena pela quantidade de jogos por plataforma, separando cada ano
+db_plataforma_ano.sort_values(by='Quantidade de Jogos', ascending=False)
+
+#grafico mostrando a quantidade de jogos por plataforma, somados
+plt.figure(figsize=(15, 8))
+sns.countplot(y="Plataforma", data=db_ano, order=db_ano['Plataforma'].value_counts().index)
+plt.title('Quantidade de Jogos por Plataforma')
+plt.show()
+
+#grafico interativ, mostrando a quantidade de jogos por plataforma, ano a ano
+fig = px.line(db_plataforma_ano, x=' Ano de Lançamento', y='Quantidade de Jogos', color='Plataforma', title='Comparação de Plataformas ao Longo dos Anos')
+fig.update_layout(xaxis_title='Ano de Lançamento', yaxis_title='Quantidade de Jogos')
+fig.show()
