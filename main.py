7060ac9a698386b1f2be3e0aa8010e5e43eeaec3
MatheusFernandesCarillo <75450635+MatheusFernandesCarillo@users.noreply.github.com>
2025-02-05 18:03:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-db = pd.read_csv('https://github.com/MatheusFernandesCarillo/jogos-analise/blob/main/Video_Games_Sales_as_at_22_Dec_2016.csv')
+db = pd.read_csv('https://raw.githubusercontent.com/MatheusFernandesCarillo/jogos-analise/refs/heads/main/Video_Games_Sales_as_at_22_Dec_2016.csv')
 
 db.columns = ["Nome", "Plataforma", "Lançamento", "Genero", "Publicadora", "Vendas_EUA", "Vendas_Europa", "Vendas_Japão", "Vendas_Outros", "Vendas_Global", "Nota_Critica", "Contagem_Criticos", "Nota_Usuario", "Contagem_Usuarios", "Desenvolvedora", "Classificação"]
 
